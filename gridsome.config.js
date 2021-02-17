e74670db959c58a1fe4d00cf798e7e210d6b72da
Alan Sanchez <sanchezpineda03@gmail.com>
2021-02-16 19:30:56 -0600

    Global stylus resources.

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,36 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styl/vars.styl'),
+        path.resolve(__dirname, './src/assets/styl/mixins.styl'),
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [],
+  css: {
+    loaderOptions: {
+      stylus: {
+        test: /\.styl$/,
+        loader: 'stylus-loader'
+      }
+    }
+  },
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('stylus').oneOf(type))
+    })
+  }
 }
