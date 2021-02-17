c7dd89a8cc5c0fcdb1b4524f76d24bf76d9fc0a4
Alan Sanchez <sanchezpineda03@gmail.com>
2021-02-16 19:33:32 -0600

    Adds normalize.css and global styles to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import 'normalize.css' // Remove if using a css framework, those already normalize styles
+import '~/assets/styl/global.styl'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
