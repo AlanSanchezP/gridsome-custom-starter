923ac2765f5b1466a88f77ca7608d5849f218874
Alan Sanchez <sanchezpineda03@gmail.com>
2021-02-25 18:43:26 -0600

    Contact form basic validations.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,27 +1,61 @@
 <template>
   <Layout class="contact-form-page">
     <h1 class="page-title">Contact us</h1>
-    <form class="contact-form">
-      <div class="input-wrapper input-wrapper--short-contact-form-size">
-        <input id="name" class="contact-form__input" type="text" name="name" placeholder="Name">
+    <form class="contact-form" @submit.prevent="submit">
+      <div class="form-item form-item--short-contact" :class="{ 'form-item--error': $v.name.$error}">
+        <input class="form-item__input" type="text" name="name" placeholder="Name" v-model.trim="$v.name.$model">
+        <div class="form-item__message" v-if="!$v.name.required && $v.name.$dirty">Field is required.</div>
       </div>
-      <div class="input-wrapper input-wrapper--short-contact-form-size">
-        <input id="email" class="contact-form__input" type="email" name="email" placeholder="Email">
+      <div class="form-item form-item--short-contact" :class="{ 'form-item--error': $v.email.$error}">
+        <input class="form-item__input" type="email" name="email" placeholder="Email" v-model.trim="$v.email.$model">
+        <div class="form-item__message" v-if="!$v.email.required && $v.email.$dirty">Field is required.</div>
+        <div class="form-item__message" v-if="!$v.email.email && $v.email.$dirty">Field must be an email.</div>
       </div>
-      <div class="input-wrapper">
-        <textarea id="message" class="contact-form__input" name="message" placeholder="Message"></textarea>
+      <div class="form-item" :class="{ 'form-item--error': $v.message.$error}">
+        <textarea class="form-item__input" name="message" placeholder="Message" v-model.trim="$v.message.$model"></textarea>
+        <div class="form-item__message" v-if="!$v.message.maxLength">Message should be less than {{$v.message.$params.maxLength.max}} characters.</div>
       </div>
-      <div class="button-wrapper">
-        <button class="site-button site-button--default" type="submit" name="submit">Enviar</button>
+      <div class="form-submit">
+        <button class="site-button site-button--default" type="submit" name="submit">Submit</button>
       </div>
     </form>
   </Layout>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate';
+import { required, email, maxLength } from 'vuelidate/lib/validators';
+
+const PENDING = -1;
+const INVALID = 0;
+const SUCCESS = 1;
+
 export default {
   metaInfo: {
     title: 'Contact us'
+  },
+  mixins: [validationMixin],
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      submitStatus: null
+    }
+  },
+  validations: {
+    name : {required},
+    email: {required, email},
+    message: {
+      maxLength: maxLength(100)
+    }
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+      this.submitStatus = !this.$v.$error;
+      // TODO: Handle actual submission here
+    }
   }
 }
 </script>
@@ -37,6 +71,10 @@ export default {
   center-block()
   max-width 436px
   width 90%
+
+.form-item
+  margin-top remify(14)
+  font-size remify(17)
 
   &__input
     color $inputTextColor
@@ -54,24 +92,19 @@ export default {
       font-family inherit
       font-weight $normal
 
-.button-wrapper
-  margin-top remify(25)
+    textarea&
+      height 180px
 
-.input-wrapper
-  margin-top remify(14)
-  font-size remify(17)
+.form-submit
+  margin-top remify(25)
 
 .site-button
   font-size remify(16)
 
-textarea.contact-form__input
-  height 180px
-
 @media screen and (min-width: 481px)
-  .contact-form
-    .button-wrapper
-      text-align right
+  .form-item--short-contact
+    width 70%
 
-    .input-wrapper--short-contact-form-size
-      width 70%
+  .form-submit
+    text-align right
 </style>
