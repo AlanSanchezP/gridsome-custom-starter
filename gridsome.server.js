722f1067381f448f04a5137b6bb65b46ee5bb749
Alan Sanchez <sanchezpineda03@gmail.com>
2021-03-05 18:14:16 -0600

    Moves collection data to yml file.

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,22 +5,19 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+const itemsFileContent = fs.readFileSync('./src/data/items.yml', 'utf8');
+const itemsData = yaml.load(itemsFileContent);
+
 module.exports = function (api) {
   api.loadSource(actions => {
     const items = actions.addCollection('Item');
 
-    items.addNode({
-      id: 0,
-      title : 'Esse inventore, eveniet eligendi',
-      thumbnail : '~/assets/img/logo/logo.png',
-      description: `Quos reprehenderit ducimus esse cumque veniam adipisci. Labore soluta deleniti repudiandae nisi non fuga autem odio debitis alias hic, dicta dolorem eum doloribus minus! Aut sit illum commodi sint quas.`
-    });
-    items.addNode({
-      id: 1,
-      title: 'Doloreq cumque voluptatum',
-      thumbnail: '~/assets/img/logo/logo.png',
-      description: `Modi ratione magni veniam laudantium architecto excepturi facere pariatur consequuntur voluptatum cupiditate veritatis sequi natus! Quaerat repellendus facilis error placeat blanditiis ea.`
-    });
+    for (const item of itemsData) {
+      items.addNode(item);
+    }
   });
 
   api.createPages(({ createPage }) => {
